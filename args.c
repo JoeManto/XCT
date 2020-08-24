0af5fcd012535ff5e770578fc3012c4bf04cc88f
Joe Manto <joemanto123@gmail.com>
2020-08-23 22:29:41 -0400

    Added functions to assign arg props and moved args and enums def to header file

--- args.c
+++ args.c
@@ -2,39 +2,12 @@
 #include "xct.h"
 
 extern int opterr;
-
-enum Matcher {
-  exact,
-  substring,
-  regex,
-};
-
-enum Project {
-  workspace,
-  project,
-};
-
-typedef struct Arguments {
-    enum Matcher matchingType;
-    enum Project projectType;
-    char* testTargetFile;
-    char* projectTarget;
-    char* matchingString;
-    char* scheme;
-    char* os;
-    char* device;
-    int dry;
-    int ugly;
-    int clear;
-} args;
-
 args * PRO_ARGS;
 
 args* newArgs() {
   args* arguments = malloc(sizeof(struct Arguments));
   if(arguments == NULL){
-    perror("Memory Error");
-    exit(1);
+    exitOnError("Memory Error");
   }
 
   // Alloc defaults
@@ -54,20 +27,20 @@ void deinit_args() {
   PRO_ARGS = NULL;
 }
 
-int parseArgs(int argc, char** argv){
-
+int parseArgs(int argc, char** argv) {
     init_args();
 
     int opt;
+    // Set opterr to avoid external errors
     opterr = 0;
 
-    while((opt = getopt(argc, argv, "t:e:s:d:P:S:O:D:")) != EOF) {
+    while((opt = getopt(argc, argv, ":t:e:s:d:P:S:O:D:")) != EOF) {
         switch(opt) {
             case 'e':
             case 's':
             case 'd':
                 printf("match option: %c\n", opt);
-                args_setMatchingType(optarg, opt);
+                opterr = args_setMatchingProps(optarg, opt);
                 break;
             case 't':
                 printf("testTargetFile: %s\n", optarg);
@@ -93,13 +66,21 @@ int parseArgs(int argc, char** argv){
             default:
               abort();
         }
+
+        if(!opterr){
+          return opterr;
+        }
     }
 
     args_describe();
     return 0;
 }
 
-void args_setMatchingType(char * matchingString, char type){
+int args_setMatchingProps(char * matchingString, char type) {
+  if(matchingString == NULL) {
+      return 1;
+  }
+
   // Set matchingType enum in args
   enum Matcher matcher = regex;
   switch(type){
@@ -113,24 +94,67 @@ void args_setMatchingType(char * matchingString, char type){
 
   PRO_ARGS->matchingType = matcher;
 
-  // If a matching string already exists free & nullify
-  if(PRO_ARGS->matchingString != NULL){
-    free(PRO_ARGS->matchingString);
-    PRO_ARGS->matchingString = NULL;
-  }
+  clearPropIfNeeded(PRO_ARGS->matchingString);
+  args_assignStringProp(PRO_ARGS->matchingString, matchingString);
 
-  PRO_ARGS->matchingString = malloc(strlen(matchingString) * sizeof(char));
-  strcpy(PRO_ARGS->matchingString, matchingString);
+  return 0;
 }
 
+int args_setTestTargetFileProp(char * target) {
+    if(target == NULL){
+      return 1;
+    }
+    clearPropIfNeeded(PRO_ARGS->testTargetFile);
+    args_assignStringProp(PRO_ARGS->testTargetFile, target);
+}
 
-void args_describe(){
+int args_setProjectProps(char * projectTarget) {
+    if(target == NULL){
+      return 1;
+    }
+
+    char * tok = strtok(string, ".");
+    tok = strtok(NULL, ".");
+
+    // ProjectTarget doesnt have required delim
+    if(tok == NULL){
+      return 1;
+    }
+
+    // Set args projectType enum to the correct value
+    char * projectType = tok;
+    if(strcmp(projectType, "xcodeproj")){
+        PRO_ARGS->projectType = project;
+    }else if (strcmp(projectType, "xcworkspace")){
+        PRO_ARGS->projectType = workspace;
+    }else{
+      return 1;
+    }
+
+    clearPropIfNeeded(PRO_ARGS->projectTarget);
+    args_assignStringProp(PRO_ARGS->projectTarget, projectTarget);
+}
+
+int args_assignStringProp(char* dst, char* src){
+  dst = malloc(strlen(src) * sizeof(char));
+  strcpy(dst, src);
+}
+
+// Expects value to be not be NULL
+// free and nullifies the given ptr
+void args_clearPropIfNeeded(char * ptr) {
+  if(ptr != NULL){
+    free(ptr);
+    ptr = NULL;
+  }
+}
+
+void args_describe() {
   if(PRO_ARGS == NULL){
      printf("%s\n","Can't print args: PRO_ARGS is NULL");
      return;
   }
   printf("%s\n","-----------ARGS------------");
-
   printf("matchingType: %d\n",PRO_ARGS->matchingType);
   printf("matchingString: %s\n",PRO_ARGS->matchingString != NULL ? PRO_ARGS->matchingString : "(null)");
 }
--- args.h
+++ args.h
@@ -1,9 +1,38 @@
 #ifndef ARGS_DOT_H
 #define ARGS_DOT_H
 
+enum Matcher {
+  exact,
+  substring,
+  regex,
+};
+
+enum Project {
+  workspace,
+  project,
+};
+
+typedef struct Arguments {
+    enum Matcher matchingType;
+    enum Project projectType;
+    char* testTargetFile;
+    char* projectTarget;
+    char* matchingString;
+    char* scheme;
+    char* os;
+    char* device;
+    int dry;
+    int ugly;
+    int clear;
+} args;
+
 void init_args();
-int parseArgs(int argc, char** argv);
-void args_setMatchingType(char * matchingString, char type);
+void deinit_args();
+args* newArgs();
+int  parseArgs(int argc, char** argv);
+int  args_setMatchingProps(char * matchingString, char type);
+int  args_assignStringProp(char* dst, char* src);
+void args_clearPropIfNeeded(char * ptr);
 void args_describe();
 
 #endif /* ARGS_DOT_H */

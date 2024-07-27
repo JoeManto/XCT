34b9c94dff73cd4bbb058b5a8df18881e4a12a49
JoeManto <joemanto123@gmail.com>
2024-07-26 18:02:21 -0700

    Add Swift testing file

---
+++ XCTTests/SwiftTests.swift
@@ -0,0 +1,43 @@
+//
+//  SwiftTests.swift
+//  XCTTests
+//
+//  Created by Joe Manto on 7/22/24.
+//
+
+import Foundation
+import XCTest
+
+class SwiftTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let x = 5
+        let y = 6 // this is a comment
+        let j = 5
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testExample1() throws {
+        let x = 5
+        let y = 6 // this is a comment
+        let j = 5
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testDifferentExample1() throws {
+        let x = 5
+        let y = 6 // this is a comment
+        let j = 5
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
